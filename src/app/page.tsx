69773b380b358773f6542a797ec2a56c249d01ac
Lilia Paula <liliaxtz2@gmail.com>
2023-09-19 20:44:44 -0300

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import styles from "./page.module.css";
+"use client";
+
+import Header from "@/components/Header";
+import useProducts from "@/hooks/useProducts";
 
 export default function Home() {
+  const { products, isFetching, error } = useProducts();
+  console.log("all products", products);
+  console.log("fetching products", isFetching);
+  console.log("error", error);
+
   return (
-    <main className={styles.main}>
-      <h3>hello world</h3>
+    <main>
+      <Header />
     </main>
   );
 }
