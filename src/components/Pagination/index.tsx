cc6bb5a8ac7137108ef188622671155a2fc7de75
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2023-09-21 13:47:48 -0300

    fix: joy card in conflict with the theme mui

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import PaginationBase from "@mui/material/Pagination";
+
+
+export default function Pagination() {
+    return (
+        <Box display={'flex'} justifyContent={'center'} mt={3} mb={3}>
+            <PaginationBase count={10} />
+        </Box>
+    )
+}
